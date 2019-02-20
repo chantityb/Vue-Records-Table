f0297b541e79f2e3e184e5dcdcca4717384741af
chantityb <chantityb@gmail.com>
2019-02-20 06:47:50 -0500

    added css

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
     <h1>{{ msg }}</h1>
-    <input type="text" v-model="search" placeholder=" search records">
   
     <table class="table mt-4">
       <thead class="thead-dark">
@@ -10,14 +9,16 @@
           <th scope="col">Title</th>
           <th scope="col">Phone Number</th>
           <th scope="col">Web Address</th>
+          <th scope="col">State</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody class="table-dark">
         <tr v-for="record in records" :key="record.CHFNM">
           <td>{{record.CHFNM}}</td>
           <td>{{record.CHFTITLE}}</td>
           <td>{{record.GENTELE}}</td>
           <td>{{record.WEBADDR}}</td>
+          <td>{{record.STABBR}}</td>
         </tr>
       </tbody>
     </table>
@@ -39,28 +40,34 @@ export default {
       }
     },
 
-    mounted() {
+    created() {
     axios
       .get('https://inventory.data.gov/api/action/datastore_search?resource_id=38625c3d-5388-4c16-a30f-d105432553a4&q=KY&limit=120')
       .then(response => (this.records = response.data.result.records))
-  }
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+
+
+body {
+  font-size: 14px;
+  color: #444;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+table {
+  border-radius: 3px;
+  background-color: #fff;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.table .thead-dark th {
+  color: #fff;
+  background-color: #ad0000;
+  border-color: #ad0000;
 }
-a {
-  color: #42b983;
-}
+
+
 </style>
